37c31019e90bd797ebcb0b1d9f741dce38d6ce5a
Yeyan Wang <120543690+yeyanwang@users.noreply.github.com>
2023-02-15 18:35:08 -0800

    Fixed Indentation

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -8,7 +8,7 @@ CREATE TABLE dept_emp (
 	emp_no INT NOT NULL, 
 	dept_no VARCHAR(5) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
